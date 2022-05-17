fd919c2f2a80a5c9d3ed73dd0d69432a5b8047a9
yk-jp <k82.y12@gmail.com>
2022-05-17 00:52:53 -0700

    add toggle to button and navbar

--- NavbarAnimation/NavbarAnimation/ViewController.swift
+++ NavbarAnimation/NavbarAnimation/ViewController.swift
@@ -9,8 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let NAVBAR_HEIGHT:[String:CGFloat] = [
+        "before": 88.0,
+        "after": 200.0
+    ]
+    
+    var isOpen = false
+    
     @IBOutlet var navBar: UIView!
     
+    @IBOutlet var plusButton: UIButton!
     @IBOutlet var navBarHeightConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
@@ -20,11 +28,28 @@ class ViewController: UIViewController {
     
     @IBAction func plusPressed(_ sender: UIButton) {
         UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.4 ,initialSpringVelocity: 0.2, animations: {
-            //            self.navBar.frame.size.height = 200
-            self.navBarHeightConstraint.constant += (200-self.navBarHeightConstraint.constant)
+            if !self.isOpen {
+                self.navBarHeightAnimation(height: self.NAVBAR_HEIGHT["after"]! - self.NAVBAR_HEIGHT["before"]!)
+                self.plusBtnAnimation(degree: .pi/4)
+            } else {
+                self.navBarHeightAnimation(height: -(self.NAVBAR_HEIGHT["after"]! - self.NAVBAR_HEIGHT["before"]!))
+                self.plusBtnAnimation(degree: -1 * .pi/4)
+                
+            }
+            
+            self.isOpen = !self.isOpen
             self.view.layoutIfNeeded()
         }
         )
+    }
+    
+    private func plusBtnAnimation(degree: CGFloat) {
+        let rotateTransform = CGAffineTransform(rotationAngle: degree)
+        self.plusButton.transform = rotateTransform
+    }
+    
+    private func navBarHeightAnimation(height: CGFloat) {
+        self.navBarHeightConstraint.constant += height
     }
     
 }
